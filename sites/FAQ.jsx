b9b5bf0a9beef33748978b2253ff59d04ef52f46
jose <hernandezjos1121@gmail.com>
2023-06-28 15:53:46 -0400

    trece push

--- sites/FAQ.jsx
+++ sites/FAQ.jsx
@@ -1,14 +1,43 @@
 import { ArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
-function ShowContent(ShowA) {
+function ShowContent() {
   document.getElementById("display-question").style.display = "block";
 }
 
-function toggleClass() {
-  const p = document.getElementById("display-question");
-  p.classList.toggle("SHOW");
+function StopShowing() {
+  document.getElementById("display-question").style.display = "none";
+}
+
+function ShowContent1() {
+  document.getElementById("display-question1").style.display = "block";
+}
+
+function StopShowing1() {
+  document.getElementById("display-question1").style.display = "none";
+}
+function ShowContent2() {
+  document.getElementById("display-question2").style.display = "block";
+}
+
+function StopShowing2() {
+  document.getElementById("display-question2").style.display = "none";
+}
+function ShowContent3() {
+  document.getElementById("display-question3").style.display = "block";
+}
+
+function StopShowing3() {
+  document.getElementById("display-question3").style.display = "none";
+}
+function ShowContent4() {
+  document.getElementById("display-question4").style.display = "block";
+}
+
+function StopShowing4() {
+  document.getElementById("display-question4").style.display = "none";
 }
 
 const FAQ = () => {
@@ -18,133 +47,93 @@ const FAQ = () => {
         <div className="row">
           <div className="FAQ-wrapper">
             <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
+              <h1 className="FAQ-Question ">
                 Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
                 <FontAwesomeIcon
-                  id="display-now"
+                  onClick={() => ShowContent(this)}
                   icon="fa-solid fa-arrow-down"
                 />
+                <FontAwesomeIcon
+                  onClick={() => StopShowing(this)}
+                  icon="fa-solid fa-arrow-up"
+                />
               </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
+              <h2 className="FAQ-Answear" id="display-question">
                 A: Yes,It'll Display The First 8 Movies Thats Corresponding
                 Whatever You search up!{" "}
               </h2>
             </div>
             <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
+              <h1 className="FAQ-Question ">
+                Q: How Many Pages Does Infinite Movies Have?{" "}
                 <FontAwesomeIcon
-                  id="display-now"
+                  onClick={() => ShowContent1(this)}
                   icon="fa-solid fa-arrow-down"
                 />
+                <FontAwesomeIcon
+                  onClick={() => StopShowing1(this)}
+                  icon="fa-solid fa-arrow-up"
+                />
               </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
+              <h2 className="FAQ-Answear" id="display-question1">
+                A: We Have A Total Of 8 Pages!{" "}
               </h2>
             </div>
             <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
+              <h1 className="FAQ-Question ">
+                Q: How Long Did It Take To Create This Masterpeice?{" "}
                 <FontAwesomeIcon
-                  id="display-now"
+                  onClick={() => ShowContent2(this)}
                   icon="fa-solid fa-arrow-down"
                 />
+                <FontAwesomeIcon
+                  onClick={() => StopShowing2(this)}
+                  icon="fa-solid fa-arrow-up"
+                />
               </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
+              <h2 className="FAQ-Answear" id="display-question2">
+                A: 1-2 Weeks,Only Because I Would Take Some Time Off To Relax{" "}
               </h2>
             </div>
             <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
+              <h1 className="FAQ-Question ">
+                Q: Is There Room For Improvement Into This Website?{" "}
                 <FontAwesomeIcon
-                  id="display-now"
+                  onClick={() => ShowContent3(this)}
                   icon="fa-solid fa-arrow-down"
                 />
+                <FontAwesomeIcon
+                  onClick={() => StopShowing3(this)}
+                  icon="fa-solid fa-arrow-up"
+                />
               </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
+              <h2 className="FAQ-Answear" id="display-question3">
+                A: Yes,There's Actually Some Flaws I Could'nt Solve <br />
+                Such As Transistions,Image Slider,FAQ Page And Alot More{" "}
               </h2>
             </div>
             <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
+              <h1 className="FAQ-Question ">
+                Q: Reptile Why Are You So Handsome And Very Well Dedicated?{" "}
                 <FontAwesomeIcon
-                  id="display-now"
+                  onClick={() => ShowContent4(this)}
                   icon="fa-solid fa-arrow-down"
                 />
-              </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
-              </h2>
-            </div>
-            <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
                 <FontAwesomeIcon
-                  id="display-now"
-                  icon="fa-solid fa-arrow-down"
+                  onClick={() => StopShowing4(this)}
+                  icon="fa-solid fa-arrow-up"
                 />
               </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
-              </h2>
-            </div>
-            <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
-                <FontAwesomeIcon
-                  id="display-now"
-                  icon="fa-solid fa-arrow-down"
-                />
-              </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
-              </h2>
-            </div>
-            <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
-                <FontAwesomeIcon
-                  id="display-now"
-                  icon="fa-solid fa-arrow-down"
-                />
-              </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
-              </h2>
-            </div>
-            <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
-                <FontAwesomeIcon
-                  id="display-now"
-                  icon="fa-solid fa-arrow-down"
-                />
-              </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
-              </h2>
-            </div>
-            <div className="FAQ-text-wrapper">
-              <h1 onClick={() => ShowContent(this)} className="FAQ-Question ">
-                Q: Does Infinite Movies Include A Functioning Search Bar?{" "}
-                <FontAwesomeIcon
-                  id="display-now"
-                  icon="fa-solid fa-arrow-down"
-                />
-              </h1>
-              <h2 className="FAQ-Answear" value="SHOW" id="display-question">
-                A: Yes,It'll Display The First 8 Movies Thats Corresponding
-                Whatever You search up!{" "}
+              <h2 className="FAQ-Answear" id="display-question4">
+                A: Well,Reptile Is Actually A top dawg,powerhouse,ruler,
+                dominator, placer, overdog, subjugator, champ, champion,
+                conqueror,But Most Importantly Here To Succeed. IF ANYONE IS
+                READING THIS CHECK MY{" "}
+                <Link to="/ReptileLetter">
+                  <span className="motivation-letter-link">
+                    MOTIVATION LETTER
+                  </span>
+                </Link>
               </h2>
             </div>
           </div>
---
+++ sites/Reptile-Letter.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const ReptileLetter = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="row">
+          <p className="letter">
+            if you are reading this you more likely starting week 6 or
+            finished.But since week 6 is the last week completed/soon to complte
+            I wanted to give off my experience in the beginning of the journey.
+            <br /> <br /> Week 1 and week 2 were the best weeks by far starting
+            html and css were extremly enjoyable and doing this project and as
+            im writing this down brings me happiness,Ive never thought of
+            actually doing something that'll soon be my oneiric(dream) to be
+            doing.
+            <br />
+            <br />
+            Week 3 was the beginning of a wild ride,starting javascript from
+            sctrach was a total "wtf" and I had thoughts of "I dont know if this
+            is for me" and started to doubt myself,The week 4 project made me
+            never wanted to code again so I took a week off to get rid of my
+            negative engergy,Afterwards I looked back at my week 2 project and
+            as well "testimonials" to help bring my motivation back.
+            <br />
+            <br />I started off week 5 very very very forceful I just imagined
+            myself as the person on top of the food chain in FrontEndSimplified
+            as well thought of amazing creations I could be able to construct
+            and hey with the knowledge and power you've gathered who knows what
+            $ could come to you! As I am writing this,Im still creating my week
+            6 project and I want it to give my full potential.YES! theres still
+            some things I dont know,Although I attempted to create a imageslider
+            (in Home) but unfortunately couldnt this project will still satisfy
+            because I gave it my all even if theres still flaws.
+            <br />
+            <br />
+            If you're wanting to get to know me more,Im Reptile Im 16,Still in
+            school but currently in summer break(soon to be a senior),Im doing
+            full time as im doing FES!
+            <br />
+            <br />
+            Moral Of The Story Dont Give Up! I Know Theres A Pot Of Gold At The
+            End Of Rainbow For You And I!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default ReptileLetter;
