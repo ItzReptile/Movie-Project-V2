4f7c51f48b3792eb4d1e85b24d272dfe8c8d1099
jose <hernandezjos1121@gmail.com>
2023-06-30 00:19:51 -0400

    almost there

--- sites/Home.jsx
+++ sites/Home.jsx
@@ -3,11 +3,15 @@ import Landing from '../components/Landing'
 import Movies from '../components/Movies'
 import TvShows from '../components/TvShows'
 import Must from '../components/Must'
+import Search from '../components/Search'
+import Results from '../components/Results'
 
 const Home = () => {
     return (
         <>
         <Landing/>
+        <Search/>
+        <Results/>
         <Movies />
         <TvShows/>
         <Must/>
